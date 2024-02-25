636fe2ad4da383fbc4273e8d246f63f88ca1ad8d
Kexin Xie <kexinx@vt.edu>
2024-02-25 13:24:32 -0500

    update loss

--- R/loss.R
+++ R/loss.R
@@ -2,8 +2,7 @@ loss <- function(y,yhat,family,type.measure=c("deviance","class")){
   if(type.measure=="deviance"){
       val <- array(NA, dim = dim(yhat))
     if (family=="binomial") {
-      if (sum(y==1)) val[y==1,,] <- -2*log(yhat[y==1, , ,drop=FALSE])
-      if (sum(y==0)) val[y==0,,] <- -2*log(1-yhat[y==0, , ,drop=FALSE])
+      val <- -2*(y*log(yhat)+(1-y)*log(1-yhat))
      }  else if (family=="poisson") {
       yly <- y*log(y)
       yly[y==0] <- 0
